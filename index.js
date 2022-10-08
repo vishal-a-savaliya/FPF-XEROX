cabd59477bf3f413bc94528efd42799dbe5120ae
rutvikgondaliya <gondaliyarutvik281@gmail.com>
2022-10-08 15:16:11 +0530

    change the root endpoint

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index");
+    res.send("FPF-XEROX-API");
 })
 
 const auth = new google.auth.GoogleAuth({
